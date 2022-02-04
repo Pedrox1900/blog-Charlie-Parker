d3111d156d07ce47e883e713550ee3075f4a7d89
Pedrox1900 <94938939+Pedrox1900@users.noreply.github.com>
2022-02-04 20:50:13 -0300

    Update conexão.php

--- blog/conexão.php
+++ blog/conexão.php
@@ -1,12 +1,11 @@
 <?php
 
 $hostname = "localhost";
-$bancodedados = "blog"
+$bancodedados = "blog";
 $usuario = "root";
 $senha = "";
 
-$mysql = new mysql($hostname, $usuario, $senha, $bancodedados);
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 if ($mysqli->connect_errno) {
     echo "falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-?>
